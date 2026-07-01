47ef074a8063f4d58aeb67d20259331a51ac8cb5
rockernun <byunguk98@naver.com>
2026-07-01 17:08:25 +0900

    feat: helloStep에 Step ExecutionContext 저장 예제 추가

--- src/main/java/com/rockernun/batch/HelloJobConfig.java
+++ src/main/java/com/rockernun/batch/HelloJobConfig.java
@@ -5,6 +5,7 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.ExecutionContext;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,14 +15,30 @@ import org.springframework.transaction.PlatformTransactionManager;
 public class HelloJobConfig {
 
     @Bean
+    // 하나의 Job은 JobRepository와 Step을 파라미터로 받음
     public Job helloJob(JobRepository jobRepository, Step helloStep) {
         return new JobBuilder("helloJob", jobRepository).start(helloStep).build();
     }
 
+//    @Bean
+//    public Step helloStep(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+//        return new StepBuilder("helloStep", jobRepository).tasklet((contribution, chunkContext) -> {
+//            System.out.println("hello spring batch!");
+//            return RepeatStatus.FINISHED;
+//        }, transactionManager).build();
+//    }
+
     @Bean
     public Step helloStep(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
         return new StepBuilder("helloStep", jobRepository).tasklet((contribution, chunkContext) -> {
-            System.out.println("hello spring batch!");
+
+            // 현재 Step의 Execution Context를 가져옴 -> 저장
+            ExecutionContext stepContext = chunkContext.getStepContext()
+                            .getStepExecution().getExecutionContext();
+            stepContext.put("processedAt", "2026-07-01");
+            stepContext.put("count", 100);
+
+            System.out.println("Hello Spring Batch Execution!");
             return RepeatStatus.FINISHED;
         }, transactionManager).build();
     }
