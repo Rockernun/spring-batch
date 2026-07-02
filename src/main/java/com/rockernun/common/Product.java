73d3ac21a31009618a601cc40c4ef96d65110d1b
rockernun <byunguk98@naver.com>
2026-07-02 11:12:16 +0900

    feat: JDBC 매핑용 Product 도메인 클래스 추가

---
+++ src/main/java/com/rockernun/common/Product.java
@@ -0,0 +1,40 @@
+package com.rockernun.common;
+
+public class Product {
+    private Long id;
+    private String name;
+    private int price;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
